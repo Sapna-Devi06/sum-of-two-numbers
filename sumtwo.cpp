3fa203967aa11c657ea07899486329dc97838af3
Sapna-Devi06 <nd747654@gmail.com>
2026-06-23 16:27:07 +0530

    Add files via upload

---
+++ sumtwo.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the number: ";
+    cin>>n;
+    int m;
+    cout<<"enter the second the number: ";
+    cin>>m;
+    int sum=n+m;
+    
+    cout<<"The sum is : "<<sum;
+return 0;
+}
